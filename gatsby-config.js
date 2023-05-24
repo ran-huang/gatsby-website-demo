576f0334d70a4e949503f27dc236d8089f1f646d
Ran <huangran.alex@gmail.com>
2023-05-24 10:36:24 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby_starter`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
