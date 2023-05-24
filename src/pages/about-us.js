6e6726c2990ba169939aa89ef15976217760af72
Ran <huangran.alex@gmail.com>
2023-05-24 15:45:40 +0800

    tutorial
    Signed-off-by: Ran <huangran.alex@gmail.com>

---
+++ src/pages/about-us.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Houlang">
+      <p>Hi there! We're a group of professional lawyers that help people.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => {
+  <title>About Houlang</title>
+}
+
+export default AboutPage
